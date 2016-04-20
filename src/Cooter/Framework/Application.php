27cccfa7a5c4b5770be873e4b81a6fb1860cf58f
Jonjo McKay <jonjo@jonjomckay.com>
2016-04-20 23:18:20 +0100

    Added ability to set a strategy

--- src/Cooter/Framework/Application.php
+++ src/Cooter/Framework/Application.php
@@ -6,6 +6,7 @@ use Franzl\Middleware\Whoops\WhoopsRunner;
 use League\Container\Container;
 use League\Container\ReflectionContainer;
 use League\Route\RouteCollection;
+use League\Route\Strategy\StrategyInterface;
 use Zend\Diactoros\Response;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
@@ -19,9 +20,9 @@ class Application
     private $container;
 
     /**
-     * @var Route[]
+     * @var RouteCollection
      */
-    private $routes = [];
+    private $routes;
 
     /**
      * @var callable[]
@@ -34,6 +35,7 @@ class Application
     public function __construct()
     {
         $this->container = new Container();
+        $this->routes = new RouteCollection($this->container);
     }
 
     public function addMiddleware(callable $middleware)
@@ -43,7 +45,12 @@ class Application
 
     public function addRoute($url, $method = 'GET', $controller, $function)
     {
-        $this->routes[$method . $url] = new Route($url, $method, $controller, $function);
+        $this->routes->map($method, $url, [$controller, $function]);
+    }
+
+    public function setStrategy(StrategyInterface $strategy)
+    {
+        $this->routes->setStrategy($strategy);
     }
 
     /**
@@ -62,17 +69,12 @@ class Application
             return ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
         });
 
-        $routes = new RouteCollection($this->container);
-        foreach ($this->routes as $route) {
-            $routes->map($route->getMethod(), $route->getUrl(), [$route->getController(), $route->getFunction()]);
-        }
-
         foreach ($this->middleware as $middleware) {
-            $routes->middleware($middleware);
+            $this->routes->middleware($middleware);
         }
 
         try {
-            $response = $routes->dispatch($this->container->get('request'), $this->container->get('response'));
+            $response = $this->routes->dispatch($this->container->get('request'), $this->container->get('response'));
 
             $emitter = new SapiEmitter();
             $emitter->emit($response);
